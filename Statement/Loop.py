e239649bf660af2c7c1cc4e14a8a8680392618be
KentaYamada <kntx.fullspeed.428@gmail.com>
2014-08-06 16:11:02 +0900

    Loopの文法確認

---
+++ Statement/Loop.py
@@ -0,0 +1,18 @@
+# -*-coding utf-8 -*-
+
+#Loop処理の確認
+#この例はCやjavaでいうfor(int i=0; i < 10; i++)と同等
+def ForLoop():
+    for i in range(10):
+        print(i)
+
+def WhileLoop():
+    i = 0;
+    while i <=100:
+        print(i)
+        i += 1
+ 
+if __name__ == "__main__":
+    ForLoop()
+    
+    WhileLoop()
